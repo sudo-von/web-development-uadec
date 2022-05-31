0b106ed11ce3cca48484354ccaad215a6654091c
VoN <sudo.von.contact@gmail.com>
2022-05-31 03:00:38 -0500

    feat: :sparkles: Added login, handled protected and public routes

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -1,45 +1,19 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Company from 'src/screens/Company/Company';
-import Home from 'src/screens/Home/Home';
-import Requirements from 'src/screens/Requirements/Requirements';
-import Sale from 'src/screens/Sale/Sale';
-import Buy from 'src/screens/Buy/Buy';
-import Contact from 'src/screens/Contact/Contact';
-import Login from 'src/screens/Login/Login';
-import PrivacyPolicies from 'src/screens/PrivacyPolicies/PrivacyPolicies';
-import QualityPolicies from 'src/screens/QualityPolicies/QualityPolicies';
-import FrequentQuestions from 'src/screens/FrequentQuestions/FrequentQuestions';
-import Tips from 'src/screens/Tips/Tips';
-import Iso from 'src/screens/Iso/Iso';
+import { BrowserRouter } from 'react-router-dom';
+import { useAuth } from 'src/contexts/auth.context';
+import PublicRoutes from './Components/PublicRoutes/PublicRoutes';
+import ProtectedRoutes from './Components/ProtectedRoutes/ProtectedRoutes';
+
+export interface RouteProps {
+  path: string;
+  element: React.ReactNode;
+}
 
 const Router = (): JSX.Element => {
+  const { authState } = useAuth();
+  const { isLoggedIn } = authState;
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/company" element={<Company />} />
-        <Route path="/services" element={<Home />} />
-        <Route path="/requirements" element={<Requirements />} />
-        <Route path="/contacts" element={<Contact />} />
-        <Route path="/search" element={<Home />} />
-        <Route path="/admin" element={<Login />} />
-        <Route path="/buy" element={<Buy />} />
-        <Route path="/build" element={<Home />} />
-        <Route path="/sale" element={<Sale />} />
-        <Route path="/moving" element={<Home />} />
-        <Route path="/insurance" element={<Home />} />
-        <Route path="/privacy-policies" element={<PrivacyPolicies />} />
-        <Route path="/frequent-questions/faq" element={<FrequentQuestions />} />
-        <Route
-          path="/frequent-questions/quality-policies"
-          element={<QualityPolicies />}
-        />
-        <Route path="/frequent-questions/tips" element={<Tips />} />
-        <Route path="/frequent-questions/iso" element={<Iso />} />
-        <Route path="/news/echo-houses" element={<Home />} />
-        <Route path="/news/isste" element={<Home />} />
-        <Route path="/news/foreign" element={<Home />} />
-      </Routes>
+      {isLoggedIn ? <ProtectedRoutes /> : <PublicRoutes />}
     </BrowserRouter>
   );
 };
